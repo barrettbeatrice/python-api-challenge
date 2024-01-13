796e6ce69110c38e1274df29b81d4751ae4b4fac
barrettbeatrice <151100165+barrettbeatrice@users.noreply.github.com>
2024-01-13 01:14:43 -0600

    Add files via upload

---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,7 @@
+# OpenWeatherMap API Key
+weather_api_key = "902d3bdf506f1f59548610dda7c1f3f8"
+
+# Geoapify API Key
+geoapify_key = "b3a68abeb125433fa2efe92350261519"
+
+#gmaps_key = "AIzaSyCL8DbZL61i8lWFNLtQ9uqmBVks2f1pWOg"
